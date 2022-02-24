690de6f7d99c72607480ee659f5d996774b2ebf8
FrogyPogy <100332786+FrogyPogy@users.noreply.github.com>
2022-02-24 11:20:24 +0700

    Add files via upload

---
+++ HelloWorld.c
@@ -0,0 +1,22 @@
+/** Nama File: HelloWorld.c **/
+/** Deskripsi: Mencetak Hello World di layar **/
+/** Nama Pembuat: Najib Rifai Indrayanto **/
+/** Tanggal Pembuatan: 24-02-2022 10:51 **/
+
+#include <stdio.h>
+
+//Main Program
+
+int main()
+{
+    //Library
+        int i; //For increment looping
+    //Algorithm
+        i = 0;
+        while(i <= 9)// loop 10 kali
+        {
+            printf("Hello World!\n");
+            i++;
+        }
+    return 0;
+}
