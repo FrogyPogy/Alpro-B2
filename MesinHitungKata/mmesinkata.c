51b43d605843b4952b3bbd04cb2f030e5fc8782d
FrogyPogy <100332786+FrogyPogy@users.noreply.github.com>
2022-05-19 21:56:16 +0700

    Update mmesinkata.c

--- MesinHitungKata/mmesinkata.c
+++ MesinHitungKata/mmesinkata.c
@@ -1,3 +1,8 @@
+/* Program   : mmesin.c */
+/* Deskripsi : Driver Program mesinkata.c untuk menghitung jumlah kata dalam pitakarakter */
+/* NIM/Nama  : 24060120140082 Najib Rifai Indrayanto*/
+/* Tanggal   : 19 Mei 2022*/
+
 #include<stdio.h>
 #include<conio.h>
 #include"mesinkata.h"
